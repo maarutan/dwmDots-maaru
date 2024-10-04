c52e0b8206ccedf86c353d5c64aa6b8a6cdd2ddf
maaru <maratarzymatov288@gmail.com>
2024-10-05 05:21:46 +0600

    add

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -1,13 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx   = 5;        /* border pixel of windows */
+static const unsigned int borderpx   = 4;        /* border pixel of windows */
 static const unsigned int snap       = 32;       /* snap pixel */
 
 static const unsigned int gappih     = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv     = 20;       /* vert inner gap between windows */
-static const unsigned int gappoh     = 30;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov     = 30;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappoh     = 25;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov     = 25;       /* vert outer gap between windows and screen edge */
 
 static       int smartgaps           = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar             = 1;        /* 0 means no bar */
@@ -17,8 +17,8 @@ static const int vertpad             = 0;       /* vertical padding of bar */
 static const int sidepad             = 0;       /* horizontal padding of bar */
 
 //static const char *fonts[]         = { "FiraCode Nerd Font:size=16","Font Awesome 6 Free Solid:size=16","Fira Code:size=16" };
-static const char *fonts[]           = { "Fira Code:size=17" };
-static const char dmenufont[]        = "Fira Code :size=17" ;
+static const char *fonts[]           = { "Fira Code:size=15" };
+static const char dmenufont[]        = "Fira Code :size=15" ;
 static const char col_gray1[]        = "#222222";
 static const char col_gray2[]        = "#444444";
 static const char col_gray3[]        = "#bbbbbb";
