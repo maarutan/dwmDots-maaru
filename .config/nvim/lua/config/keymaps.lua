ada41c7ddd950923fc5b04485264201b30cdc178
maaru <maratarzymatov288@gmail.com>
2024-10-11 03:36:23 +0600

    add

--- .config/nvim/lua/config/keymaps.lua
+++
@@ -1,5 +0,0 @@
--- LSP Server to use for Python.
--- Set to "basedpyright" to use basedpyright instead of pyright.
-vim.g.lazyvim_python_lsp = "pyright"
--- Set to "ruff_lsp" to use the old LSP implementation version.
-vim.g.lazyvim_python_ruff = "ruff"
--- suckless/scripts/gay.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-mpv --vo=kitty --no-terminal --geometry=100% $HOME/Videos/gay/gay.mp4
-
