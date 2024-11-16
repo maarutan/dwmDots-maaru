95d3e21e6648df8460d0ac965fe4780f6a92f972
maaru <maratarzymatov288@gmail.com>
2024-11-17 00:29:21 +0600

    rm -rf nvim

--- .config/nvim/lua/core/colors.lua
+++
@@ -1,8 +0,0 @@
-vim.opt.termguicolors = true
-
-function SetColor(color)
-    color = color or "catppuccin-mocha"
-    vim.cmd.colorscheme(color)
-end
-
-SetColor("catppuccin-mocha")
--- .config/nvim/lua/plugins/comments.lua
+++
@@ -1,4 +0,0 @@
-require("nvim_comment").setup({
-	line_mapping = "<leader>cl",
-	operator_mapping = "<leader>c",
-})
