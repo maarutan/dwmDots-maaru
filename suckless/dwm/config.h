bda9f3fcc6dcd477e55a5579af16463a4b424afa
maaru <maratarzymatov288@gmail.com>
2024-11-16 15:14:58 +0600

    add

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -24,7 +24,7 @@ static int show_tag_boxes = 1;  // 1 — показывать квадратик
 //gap
 static const unsigned int gappiv     = 20;       // vert inner gap between windows 
 static const unsigned int gappih     = 20;       // horiz inner gap between windows 
-static const unsigned int gappoh     = 35;       // horiz outer gap between windows and screen edge 
+static const unsigned int gappoh     = 20;       // horiz outer gap between windows and screen edge 
 static const unsigned int gappov     = 20;       // vert outer gap between windows and screen edge 
 static       int smartgaps           = 1;        // 1 means no outer gap when there is only one window 
 static const unsigned int single_gappov = 120; // Вертикальный внешний отступ при одном окне
@@ -34,7 +34,7 @@ static const int showbar             = 1;        // 0 means no bar
 static const int topbar              = 1;        // 0 means bottom bar 
 // padding bar 
 static const int vertpad             = 20;      // vertical padding of bar 
-static const int sidepad             = 35;       // horizontal padding of bar 
+static const int sidepad             = 20;       // horizontal padding of bar 
 // font
 static const char *fonts[]           = { "FiraCode Nerd Font:size=16" };
 // color
